9e9893794d9190c8eb8e36f98f91b3d553e2f25a
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-01-05 16:24:54 +0200

    Add authentication module

---
+++ src/app/authentication/authentication-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthenticationComponent } from './authentication.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const routes: Routes = [
+  {
+    path: '', component: AuthenticationComponent, children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'sign-up', component: SignUpComponent },
+      { path: 'forget-password', component: ForgetPasswordComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthenticationRoutingModule { }
---
+++ src/app/authentication/authentication.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { AuthenticationComponent } from './authentication.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+
+
+@NgModule({
+  declarations: [
+    AuthenticationComponent,
+    LoginComponent,
+    SignUpComponent,
+    ForgetPasswordComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule
+  ]
+})
+export class AuthenticationModule { }
