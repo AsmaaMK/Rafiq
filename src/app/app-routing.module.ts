6280575db87265ea14cdbfbfd359b1dd9ec1f27c
Asmaa M. Khalaf <asmaa.m.khalaf.fci@gmail.com>
2022-02-18 18:35:21 +0200

    Add not-found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+  }, 
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from '../shared/components/not-found/not-found.component';
 
 import { AuthenticationComponent } from './authentication.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
@@ -19,6 +20,7 @@ const routes: Routes = [
       { path: 'reset-password', component: ResetPasswordComponent },
       { path: 'resend-link', component: ResendLinkComponent },
       { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '**', component: NotFoundComponent }
     ],
   },
 ];
