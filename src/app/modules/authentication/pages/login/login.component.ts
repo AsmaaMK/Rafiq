f218375c314f368e24fca42df42e5f2b19e87e17
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-03-11 21:09:48 +0200

    Add animation

--- src/app/modules/authentication/pages/login/login.component.ts
+++ src/app/modules/authentication/pages/login/login.component.ts
@@ -19,8 +19,8 @@ export class LoginComponent implements OnInit {
   passwordShowMessage = false;
   rememberMe = true;
   sendingRequest = new BehaviorSubject(false);
-  showError = new BehaviorSubject(false);
-  errorMessage = '';
+  showError = new BehaviorSubject(true);
+  errorMessage = 'email, username or password is not correct';
   
 
   constructor(private auth: AuthService, private tokenStorage: TokenStorageService, private router: Router) { }
