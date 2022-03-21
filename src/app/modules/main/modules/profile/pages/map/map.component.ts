cdf4896317b843c2ab80e633469b3bf48eb7e244
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-03-21 22:34:19 +0200

    Add map markers controler

--- src/app/modules/main/modules/profile/pages/map/map.component.ts
+++ src/app/modules/main/modules/profile/pages/map/map.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
 
+const initMarkares: Marker[] = [
+  { type: 'visited', possition: { lat: 50.254540, lng: 54.5456440 } },
+  { type: 'wish', possition: { lat: 30.254540, lng: -10.5456440 } },
+  { type: 'visited', possition: { lat: 30.339090792540066, lng: 31.236435722855088 } },
+  { type: 'wish', possition: { lat: 10.254540, lng: 34.5456440 } }
+];
+
+const markers: google.maps.Marker[] = [];
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -9,29 +18,116 @@ import { Loader } from '@googlemaps/js-api-loader';
 export class MapComponent implements OnInit {
 
   private map!: google.maps.Map;
+  cursorType: 'visited' | 'wish' | null = null;
+  markerIcons = {
+    'wish': '../../../../../../../assets/main-module/profile/wish-cursor.svg',
+    'visited': '../../../../../../../assets/main-module/profile/visited-cursor.svg'
+  }
+
+  loader = new Loader({
+    apiKey: 'AIzaSyD5Xpnp8u4QkUyNA6yRkinNCcv6n0EeKfQ'
+  });
+
   constructor() { }
-  
+
   ngOnInit(): void {
-    const mapDiv = document.getElementById("google-map");
-    let loader = new Loader({
-      apiKey: 'AIzaSyD5Xpnp8u4QkUyNA6yRkinNCcv6n0EeKfQ'
-    });
+    this.loader.load().then(() => {
 
-    loader.load().then(() => {
-      
-      if (mapDiv) {
-        console.log('loaded google maps');
-        this.map = new google.maps.Map(mapDiv, {
-          center: { lat: 30.852873830183167, lng: 32.23188898842041 },
-          zoom: 3,
-          mapId: 'f23f75a0977714e8'
-        });
-      }
+      this.mapInit({ lat: 10, lng: 20 });
 
-      const marker = new google.maps.Marker({
-        position: { lat: 30.852873830183167, lng: 32.23188898842041 },
-        map: this.map,
+      this.map.addListener("click", (mapsMouseEvent: any) => {
+        if (this.cursorType) {
+          const createdMarker = this.createMarker(this.cursorType, mapsMouseEvent.latLng);
+          markers.push(createdMarker);
+          this.disableAddMarkers();
+        }
       });
     });
   }
+
+  mapInit(latLng: LatLng): void {
+    const mapDiv = document.getElementById("google-map");
+
+    if (mapDiv) {
+      this.map = new google.maps.Map(mapDiv, {
+        mapId: 'f23f75a0977714e8',
+        center: latLng,
+        zoom: 3,
+        draggableCursor: 'grab',
+        draggingCursor: 'grabbing',
+        disableDefaultUI: true,
+        fullscreenControl: true,
+        keyboardShortcuts: false
+      });
+
+      for (let marker of initMarkares) {
+        const createdMarker = this.createMarker(marker.type, marker.possition);
+        markers.push(createdMarker);
+      }
+    }
+  }
+
+  createMarker(markerType: 'visited' | 'wish', latLng: LatLng) {
+    return new google.maps.Marker({
+      position: latLng,
+      map: this.map,
+      icon: {
+        url: this.markerIcons[markerType],
+        size: new google.maps.Size(50, 50),
+        scaledSize: new google.maps.Size(50, 50),
+        anchor: new google.maps.Point(10, 20)
+      },
+      cursor: 'default',
+      animation: google.maps.Animation.DROP,
+    });
+  }
+
+  enableAddVisitedMarker(btn: HTMLElement) {
+    this.cursorType = 'visited';
+    this.enableAddMarker(this.cursorType);
+    btn.classList.add('focused');
+  }
+
+  enableAddWishMarker(btn: HTMLElement) {
+    this.cursorType = 'wish';
+    this.enableAddMarker(this.cursorType);
+    btn.classList.add('focused');
+  }
+
+  enableAddMarker(markerType: 'visited' | 'wish') {
+    this.cursorType = markerType;
+    this.map.setOptions({
+      draggableCursor: `url(${this.markerIcons[markerType]}), default`
+    });
+
+    const focusedBtn = document.querySelector('.focused');
+    if (focusedBtn) focusedBtn.classList.remove('focused');
+  }
+
+  disableAddMarkers() {
+    this.cursorType = null;
+    this.map.setOptions({
+      draggableCursor: "default"
+    });
+
+    const focusedBtn = document.querySelector('.focused');
+    if (focusedBtn) focusedBtn.classList.remove('focused');
+  }
+
+  deleteAllMarkers() {
+    for (let i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+  }
 }
+
+
+type LatLng = {
+  lat: number;
+  lng: number;
+};
+
+type Marker = {
+  type: 'visited' | 'wish';
+  possition: LatLng;
+};
