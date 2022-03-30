bca079766c18133ba847dfd434c84b2f8b62082c
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-03-30 12:50:43 +0200

    Add Edit Info UI

--- src/app/modules/main/pages/home/home.component.ts
+++ src/app/modules/main/pages/home/home.component.ts
@@ -5,26 +5,19 @@ import { TestingApiService } from 'src/app/shared/services/testing-api.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private testapi: TestingApiService, private auth: AuthService) { }
+  constructor(private testapi: TestingApiService, private auth: AuthService) {}
 
   ngOnInit(): void {
-    // this.testapi.getPosts().subscribe(
-    //   res => {
-    //     console.log('hi');
-    //   },
-    //   err => {
-    //     console.error('bye', err);
-    //   }
-    // )
-
   }
 
   logout() {
     this.auth.logoutUser();
   }
 
+  gitAccessToken() {
+    this.auth.refreshAccessToken();
+  }
 }
