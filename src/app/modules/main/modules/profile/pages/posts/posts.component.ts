6153af67a9bcf7c158183927d8ea0de41edb935a
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-04-01 17:50:29 +0200

    Fix no-posts illustration

--- src/app/modules/main/modules/profile/pages/posts/posts.component.ts
+++ src/app/modules/main/modules/profile/pages/posts/posts.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStorageService } from 'src/app/shared/services/token-storage.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
 })
 export class PostsComponent implements OnInit {
+  myUserName = this.tokenStorageService.getUsername();
+  urlUserName =  this.router.url.split('/')[3];
+  isMyProfile = this.myUserName === this.urlUserName;
 
   posts = [];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private tokenStorageService: TokenStorageService
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
