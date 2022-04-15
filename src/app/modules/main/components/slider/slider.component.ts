52233ec7728e30bb542ea4d0c004e35af4482ef5
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-04-16 01:06:38 +0200

    Fix slider and remove bootstrap

--- src/app/modules/main/components/slider/slider.component.ts
+++ src/app/modules/main/components/slider/slider.component.ts
@@ -6,12 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss'],
 })
 export class SliderComponent implements OnInit {
-  @Input() postMedia: string[] = [
-    '../../../../../assets/main-module/post-images/10fd60e3ef81dcd4c3029c86056b4f24.jpg',
-    '../../../../../assets/main-module/post-images/171885606_975972776140732_6446233969730968789_n.jpg',
-    '../../../../../assets/main-module/post-images/Albania2.png',
-    '../../../../../assets/main-module/post-images/dan-asaki-wC_YNVgIRdA-unsplash.jpg',
-  ];
+  @Input() postMedia: string[] = [];
   indexOfCurrentMedia = 0;
   numberOfMedia!: number;
 
@@ -20,7 +15,13 @@ export class SliderComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewChecked() {
+    this.setSliderWidth();
+  }
+
+  setSliderWidth() {
     this.numberOfMedia = this.postMedia.length;
     this.slider = document.getElementById('slider');
 
@@ -35,41 +36,30 @@ export class SliderComponent implements OnInit {
         '.slider__post-media'
       );
 
-      console.log(listOfPostMedia)
-
       listOfPostMedia.forEach((postMedia) => {
         postMedia.style.width =
           (this.sliderWidth / this.numberOfMedia - 144).toString() + 'px'; /// 144 = 2 * controlerSize = 2 * 72
-        
-        console.log(postMedia.style.width);
       });
     }
   }
 
   moveToPreviousMedia(): void {
-    // console.log('media: ', this.numberOfMedia);
     if (this.indexOfCurrentMedia > 0) {
       this.indexOfCurrentMedia--;
-      console.log(this.indexOfCurrentMedia);
       const translate =
         (this.sliderWidth / this.numberOfMedia) * this.indexOfCurrentMedia;
-      console.log('trans: ', translate);
-      console.log(this.slider);
+      
       if (this.slider)
         this.slider.style.transform = `translate(${-translate}px)`;
     }
   }
 
   moveToNextMedia(): void {
-    // console.log('media: ', this.numberOfMedia);
     if (this.indexOfCurrentMedia < this.numberOfMedia - 1) {
       this.indexOfCurrentMedia++;
-      console.log(this.indexOfCurrentMedia);
       const translate =
         (this.sliderWidth / this.numberOfMedia) * this.indexOfCurrentMedia;
-      console.log('trans: ', translate);
 
-      console.log(this.slider);
       if (this.slider)
         this.slider.style.transform = `translate(${-translate}px)`;
     }
