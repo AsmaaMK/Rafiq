3ca7afdcf40b23daf42fc08b6dbe3f9852901f2c
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-07-16 15:57:01 +0200

    Add about us animation

--- src/app/shared/pages/about-us/about-us.component.ts
+++ src/app/shared/pages/about-us/about-us.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent implements OnInit {
+  observer!: IntersectionObserver;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.initIntersectionObserver(null, )
   }
 
+  showImage(div: any) {
+    div.getElementsByClassName('content')[0].classList.add('show');
+    div.getElementsByClassName('image')[0].classList.add('show');
+  }
+
+  initIntersectionObserver(root: HTMLElement | null) {
+    let options = {
+      root: root,
+      rootMargin: '0px',
+      threshold: 0.5,
+    };
+
+    this.observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        this.showImage(entries[0].target);
+      }
+    }, options);
+
+    const rows = document.getElementsByClassName('row');
+    console.log(rows)
+    for (let i = 0; i < rows.length; i++) {
+      let row = rows[i];
+      this.observer.observe(row);
+    }
+  }
 }
